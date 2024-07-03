aa3680b6f04904120fcd7bb651a5627d9dcd57eb
Abhijeet Sinha <abhijeetsinha.smith@gmail.com>
2024-07-03 17:51:50 +0530

    Employee Database Managment3

--- Employee Database Managment System/index.js
+++ Employee Database Managment System/index.js
@@ -1,3 +1,5 @@
+//create an edit button which will be in information section and on clicking it will opent the modal prefilled and then you can edit the information and click on submit again which will edit the user information
+
 (async function(){
     const data = await fetch('./data.json')
     const response = await data.json()
@@ -21,13 +23,24 @@
             addEmployeeModal.style.display = "none";
         }
     })
-
+    const dobInput = document.querySelector('.addEmployee_create--dob')
+    dobInput.max = `${new Date().getFullYear() - 18}-${new Date().toISOString().slice(5,10)}`
     addEmployeeForm.addEventListener('submit',(e)=>{
         e.preventDefault()
         const formData = new FormData(addEmployeeForm)
         const values = [...formData.entries()];
-        
-        console.log(values);
+        let employeeData = {}
+        values.forEach((val)=>{
+            employeeData[val[0]] = val[1]
+        })
+        // console.log(values);
+        employeeData.id = employees[employees.length - 1].id + 1;
+        employeeData.age = new Date().getFullYear() - parseInt(employeeData.dob.slice(0,4),10)
+        employeeData.imageUrl = employeeData.imageUrl || "https://cdn-icons-png.flaticon.com/512/0/93.png"
+        employees.push(employeeData)
+        renderEmployees()
+        addEmployeeForm.reset()
+        addEmployeeModal.style.display = "none";
     })
 
     employeeList.addEventListener('click',(event)=>{
@@ -36,6 +49,16 @@
             renderEmployees()
             renderSingleEmployee()
         }
+
+        if(event.target.tagName === "I"){
+            employees = employees.filter((emp) => String(emp.id) !== event.target.parentNode.id)
+        }
+        if(String(selectedEmployeeId) === event.target.parentNode.id){
+            selectedEmployeeId = employees[0]?.id || -1;
+            selectedEmployee = employees[0] || {}
+            renderSingleEmployee()
+        }
+        renderEmployees()
     })
 
     const renderEmployees = () =>{
@@ -56,6 +79,10 @@
     }
 
     const renderSingleEmployee = ()=>{
+        if(selectedEmployeeId === -1){
+            employeeInfo.innerHTML = "";
+            return;
+        }
         employeeInfo.innerHTML = `<img src="${selectedEmployee.imageUrl}"/>
               <span class="employees__single--heading">
       ${selectedEmployee.firstName} ${selectedEmployee.lastName} (${selectedEmployee.age})
